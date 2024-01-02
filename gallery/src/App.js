1210297a62089b16419496bf4fbe525df1143d60
mwihoti <86232216+mwihoti@users.noreply.github.com>
2024-01-02 06:29:18 +0300

    food gallery

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -11,17 +11,17 @@ function App () {
   return (
     <>
     {showModal&&<Modal setShowModal={setShowModal} setSelectedContent={setSelectedContent} content={selectedContent} data={data}/>}
-    <div className='flex justify-center items-center w-full h-full flex-col bg-gray-200'>
+    <div style={styles.main}>
 <Header title= "Food Gallery" subtitle= "Simple food gallery. Check-It_Out"/>
 <div className='flex flex-wrap gap-10 max-w-60vw'>
 {data.map((image_data, index) => {
   return (
-    <div className='flex flex-col justify-center items-center content-center cursor-pointer' 
+    <div style={styles.imageContainer} 
     onClick={() => {
       setShowModal(true);
       setSelectedContent(image_data);
     }}>
-    <img className='w-150px h-120px object-cover' src={image_data.url}/>
+    <img style={styles.image} src={image_data.url}/>
     <div className='flex justify-center items-center w-full h-50px bg-black opacity-50'>
 <p className='text-white text=16px font-bold'>{image_data.name}</p><br></br>
 <p className='text-white text=16px font-bold'>{image_data.description}</p>
@@ -37,5 +37,30 @@ function App () {
     </div>
     </>
   )
+}
+const styles = {
+  main: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#f0f0f0',
+    flexDirection: "column"
+  },
+  image: {
+    width: '300px',
+    height: '300px',
+    ObjectFit: 'cover',
+  }
+  ,
+  imageContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    cursor: 'pointer',
+  }
 }
 export default App;
