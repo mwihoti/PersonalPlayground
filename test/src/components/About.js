006a6d42760fabc59d6b79c4a6819802853502cf
mwihoti <danielmwihoti@gmail.com>
2024-03-19 16:19:52 +0300

    About us

--- test/src/components/About.js
+++ test/src/components/About.js
@@ -20,15 +20,15 @@ export default function About() {
           <img src={logo} alt="Laundry logo" />
         </CardHeader>
         <CardBody>
-          <Typography variant="h5" color="blue-gray" >
+          <h3 >
             Welcome to Neno Laundry, where we redefine the way you experience laundry services. <br />
             At Neno, we understand that your clothes deserve the best care, and we're here to provide just that.
-          </Typography>
+          </h3>
 
           <div className='text-center'>
-            <h1 className=' underline font-bold mt-3'>Our Commitment:</h1>
+            <h1 className=' underline font-bold mt-3 uppercase'>Our Commitment:</h1>
 
-            <div className='flex flex-col mt-3 mb-5 text-xl'>
+            <div className='flex flex-col p-5'>
               <h1 className=' underline font-bold'> Quality Beyond Clean: </h1>
               <h4>
                 At Neno Laundry, we go beyond just cleaning clothes. We believe in providing a comprehensive service that ensures your<br /> garments
@@ -36,14 +36,14 @@ export default function About() {
               </h4>
             </div>
 
-            <div className='flex flex-col text-xl mb-5'>
+            <div className='flex flex-col leading-5 p-5'>
               <h1 className=' underline font-bold'> Quick and Convenient: </h1>
               <h4>
                 We understand that time is valuable. That's why we offer a quick turnaround on our services. Our convenient <br />
                 online booking system and optional pick-up and delivery services are designed with your busy schedule in mind.
               </h4>
             </div>
-            <div className='flex flex-col text-xl mb-5 '>
+            <div className='flex flex-col  leading-5 p-5'>
               <h1 className=' underline font-bold'>Eco-Friendly Practices: </h1>
               <h4>
                 Neno Laundry is proud to embrace environmentally friendly practices. We utilize state-of-the-art
@@ -55,12 +55,12 @@ export default function About() {
 
           </div>
 
-          <Typography variant='h6'>
-            <h1 className=' underline font-bold'> Join Us on the Journey</h1>
-            Experience laundry like never before. Join us at Neno Laundry and rediscover the joy of clean, fresh, and perfectly folded <br /> clothes. We look forward to becoming your trusted partner in garment care.
+          <div>
+            <h1 className=' underline font-bold leading-5'> Join Us </h1>
+            <h4>Experience laundry like never before. Join us  and rediscover the joy of <em>clean, fresh, and perfectly folded <br /> clothes </em> . We look forward to becoming your trusted partner in garment care.
 
-            Thank you for choosing Neno Laundry!
-          </Typography>
+            Thank you for choosing Neno Laundry!</h4>
+          </div>
 
 
         </CardBody>
