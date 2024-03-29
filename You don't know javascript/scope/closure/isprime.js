2f7a61d47b48bb03b589729e9d283cf464fcb97b
mwihoti <danielmwihoti@gmail.com>
2024-03-29 16:37:44 +0300

    Is prime

---
+++ You don't know javascript/scope/closure/isprime.js
@@ -0,0 +1,31 @@
+function isPrime(v) {
+    if (v <= 3) {
+        return v > 1;
+    }
+    if (v % 2 == 0 || v % 3)
+    {
+        return false;
+    }
+    var vsqrt = Math.sqrt(v);
+    for (let i = 5; i <= vsqrt; i += 6) {
+        if (v % i == 0 || v % (i + 2) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function factorize(v) {
+    if (!isPrime(v)) {
+        let i = Math.floor(Math.sqrtt(v));
+        while (v % i != 0) {
+            i--;
+        }
+        return [
+            ...factorize(i),
+            ...factorize(v / i)
+        ];
+
+    }
+    return [v];
+}
