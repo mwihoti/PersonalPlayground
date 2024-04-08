22a01fdd656356ea26837ad377b9b770e85c39cf
mwihoti <danielmwihoti@gmail.com>
2024-04-08 12:54:49 +0300

    QuerySelector and eventListeners

---
+++ consoles/javascriptalx/js/scripts/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = "Hello world its dante";
+
+document.querySelector("#bt").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!")
+})
