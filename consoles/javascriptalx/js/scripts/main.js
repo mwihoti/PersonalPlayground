4b2baa56377396550a4a38da69d98f12bb74c22f
mwihoti <danielmwihoti@gmail.com>
2024-04-08 12:58:45 +0300

    id $ class events

--- consoles/javascriptalx/js/scripts/main.js
+++ consoles/javascriptalx/js/scripts/main.js
@@ -4,3 +4,6 @@ myHeading.textContent = "Hello world its dante";
 document.querySelector("#bt").addEventListener("click", function () {
     alert("Ouch! Stop poking me!")
 })
+
+document.querySelector('.btc').addEventListener('click', function ()
+ {alert ("!Ouch, I am using  class instead of id") })
