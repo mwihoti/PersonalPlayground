fcc3174bdef2264a1e3d99b4b929f822a5f0e791
mwihoti <danielmwihoti@gmail.com>
2024-04-09 04:22:15 +0300

    Main events

--- consoles/javascriptalx/main.js
+++ consoles/javascriptalx/main.js
@@ -1,6 +1,12 @@
 const buttonA = document.querySelector('#button_A');
 const headingA = document.querySelector('#heading_A');
 
+//buttonA.addEventListener( 'click', () => {
+  //  const name = prompt("What is your name?");
+    //alert(`Hello ${name}, nice to see you!`);
+    //headingA.textContent = `Welcome, ${name}`;
+//})
+
 
 buttonA.addEventListener('click', () => {
     alert(`Hello ${prompt("What is your name.?")}, nice to see you!`);
