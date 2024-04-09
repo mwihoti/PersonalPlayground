7824c668c167700834c760211dfa3ee41a700087
mwihoti <danielmwihoti@gmail.com>
2024-04-09 13:18:08 +0300

    objects brackets

---
+++ You don't know javascript/objects/ob1.js
@@ -0,0 +1,19 @@
+const person = {
+    name: {first: "Bob", 
+            last: "Smith"},
+    age: 23,
+    bio () {
+        console.log(`${this.name.first} ${this.name.last} is ${this.age} years old`)
+    },
+    intro () {
+
+        console.log(`Hi i' am ${person.name.first}`)
+    }
+}
+person.intro()
+person.age = 45;
+person["name"]["last"] = "Cratchit"
+
+person.age;
+person["name"]["last"];
+person.bio()
