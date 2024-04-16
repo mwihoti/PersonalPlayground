ae84cc608db3b4bdd1c8380acb13630a689f751a
mwihoti <danielmwihoti@gmail.com>
2024-04-16 10:58:25 +0300

    sqllite databasw

--- orm/app.py
+++ orm/app.py
@@ -4,10 +4,12 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Corrected typo here
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app = create_app()
 db = SQLAlchemy(app)
+
+
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,5 +17,3 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     date_joined = db.Column(db.Date, default=datetime.utcnow)
 
-    with app.app_context():
-        db.create_all()
