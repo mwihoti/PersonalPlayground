6799bb30cdf376fba4eea9e10363edc23140dbce
mwihoti <danielmwihoti@gmail.com>
2024-05-08 07:29:41 +0300

    HeaderBox & TotalBalanceBox with react-countup

---
+++ banking-app/components/ui/HeaderBox.tsx
@@ -0,0 +1,15 @@
+const HeaderBox = ({type = "title", title, subtext, user}:HeaderBoxProps) => {
+  return (
+    <section className="header-box">
+        <h1 className="header-box-title">
+            {title}
+            {type === 'greeting' && (
+                <span className="text-bankGradient"> &nbsp;{user}</span>
+            )}
+        </h1>
+        <p className="header-box-subtext">{subtext}</p>
+    </section>
+  )
+}
+
+export default HeaderBox
---
+++ banking-app/components/ui/TotalBalanceBox.tsx
@@ -0,0 +1,29 @@
+import { formatAmount } from '@/lib/utils'
+import React from 'react'
+import AnimatedCounter from '../AnimatedCounter'
+
+const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }: TotlaBalanceBoxProps) => {
+    return (
+        <section className="total-balance">
+            <div className="total-balance-chart">
+                {/* DoughnutChart */}
+            </div>
+            <div className='flex flex-col gap-6'>
+                <h2 className="header-2">
+                Banks accounts: {totalBanks} 
+                </h2>
+                <div className='flex flex-col gap-2'>
+                    <p className='total-balance-label'>
+                        Total current balance
+                    </p>
+
+                    <p className='total-balance-amount flex-center gap-2'>
+                       <AnimatedCounter amount = {totalCurrentBalance} />
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default TotalBalanceBox
