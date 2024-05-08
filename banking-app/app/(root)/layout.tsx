515b8d2030b96c1db038b21133820a12e00931a5
mwihoti <danielmwihoti@gmail.com>
2024-05-08 13:40:29 +0300

    sidebar component

--- banking-app/app/(root)/layout.tsx
+++ banking-app/app/(root)/layout.tsx
@@ -1,11 +1,26 @@
+import Sidebar from "@/components/ui/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
     children,
 }: Readonly<{ 
     children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Daniel", lastName: "Mwihoti"};
+
+
   return (
     <main className="flex h-screen w-full font-inter">
-        SIDEBAR
+       <Sidebar user={loggedIn} />
+
+       <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+            <div>
+              
+            </div>
+          </div>
+       </div>
         {children}
     </main>
   )
