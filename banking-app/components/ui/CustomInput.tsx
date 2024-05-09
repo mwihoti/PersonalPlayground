7d52afb8f1cf48bdc35feec9f2e481661397e639
mwihoti <danielmwihoti@gmail.com>
2024-05-09 21:14:02 +0300

    Submit handler async

--- banking-app/components/ui/CustomInput.tsx
+++ banking-app/components/ui/CustomInput.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { FormControl, FormField, FormLabel, FormMessage } from './ui/form'
-import { Input } from './ui/input'
+import { FormControl, FormField, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
 
 import { Control, FieldPath } from 'react-hook-form'
 import { z } from 'zod'
