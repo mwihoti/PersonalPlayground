f17292e2101fedd34e9fef1bb4531184574ff5de
mwihoti <danielmwihoti@gmail.com>
2024-05-09 14:44:32 +0300

    sign-in & signup page

--- banking-app/app/(auth)/sign-in/page.tsx
+++ banking-app/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import AuthForm from '@/components/ui/AuthForm'
 const SignIn = () => {
   return (
-    <section className='flex-center size-full max-sm:px-6'>SignIn</section>
+    <section className='flex-center size-full max-sm:px-6'>
+
+      <AuthForm type="sign-in"/>
+    </section>
   )
 }
 
