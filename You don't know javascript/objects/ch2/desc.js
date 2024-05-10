84931325042ff235d2b23cfa3eb1a70aca01e334
mwihoti <danielmwihoti@gmail.com>
2024-05-10 05:49:54 +0300

    Property descriptors

---
+++ You don't know javascript/objects/ch2/desc.js
@@ -0,0 +1,24 @@
+myObj = {
+    favoriteNumber: 42,
+    isDeveloper: true,
+    firstName: "Kyle"
+
+};
+
+ obj = Object.getOwnPropertyDescriptor(myObj, "firstName")
+ console.log(obj)
+ obj = Object.defineProperty(myObj, "lastName", {
+    value: "Doe",
+    writable: true,
+    enumerable: true,
+    configurable: true
+ })
+ Object.defineProperty(myObj, "age", {
+    value: 30,
+    writable: true,
+    enumerable: true,
+    configurable: true
+})
+
+ 
+ console.log(obj)
