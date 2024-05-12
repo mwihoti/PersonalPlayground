b77acba37b3490b7c22c3f05270d2cf666fde48e
mwihoti <danielmwihoti@gmail.com>
2024-05-12 19:29:59 +0300

    get logged user data

--- banking-app/components/ui/AuthForm.tsx
+++ banking-app/components/ui/AuthForm.tsx
@@ -82,7 +82,7 @@ const AuthForm = ({ type }: { type: string }) => {
 
                 <div className='flex flex-col gap-1 md:gap-3'>
                     <h1 className='text-24 lg:text-36 font-semibold text-gray-900'>
-                        {user ? 'Link Account'
+                        {user ? 'Link Your Account'
                             : type === 'sign-in'
                                 ? 'Sign In'
                                 : 'Sign Up'}
