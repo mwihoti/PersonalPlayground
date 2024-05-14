482947015abedf319a197923369505838fc6cf84
mwihoti <danielmwihoti@gmail.com>
2024-05-14 10:05:32 +0300

    Defining name variable

--- banking-app/components/ui/Footer.tsx
+++ banking-app/components/ui/Footer.tsx
@@ -11,20 +11,23 @@ const Footer = ({user, type = 'desktop'}: FooterProps) => {
 
         if(loogedOut) router.push('/sign-in')
     }
+    if (!user) {
+        return null; // or return a loading indicator or placeholder
+    }
   return (
     <footer className="footer">
         <div className={type === 'mobile' ? 'footer_name-mobile':'footer_name'}>
             <p className='text-xl font-bold text-gray-700'>
-                {user.name[0]}
+                {user?.name[0]}
             </p>
            
         </div>
 
         <div className= {type === 'mobile' ? 'footer_email-mobile':'footer_email'}>
-            <h1 className='text-14 truncate font-normal text-gray-700'> {user.name}</h1>
+            <h1 className='text-14 truncate font-normal text-gray-700'> {user?.name}</h1>
 
         <p className='text-14  truncate font-normal text-gray-600 '>
-                {user.email}
+                {user?.email}
             </p>
         </div>
         <div className= 'footer_image' onClick={handleLogout}>
