e279c79134c6bef11da6b75929499dec96bb74e2
mwihoti <danielmwihoti@gmail.com>
2024-05-14 08:10:27 +0300

    footer with logout function

--- banking-app/app/(root)/layout.tsx
+++ banking-app/app/(root)/layout.tsx
@@ -1,14 +1,14 @@
 import MobileNav from "@/components/ui/MobileNav";
 import Sidebar from "@/components/ui/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: Readonly<{ 
     children: React.ReactNode;
 }>) {
-  const loggedIn = { firstName: "Daniel", lastName: "Mwihoti"};
-
+  const loggedIn = await getLoggedInUser()
 
   return (
     <main className="flex h-screen w-full font-inter">
