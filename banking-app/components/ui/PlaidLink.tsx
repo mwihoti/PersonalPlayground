8ef9ff08955f54355d8735d9e59f4221ec59fc85
mwihoti <danielmwihoti@gmail.com>
2024-05-19 12:11:12 +0300

    PlaidLink button onClick functionality

--- banking-app/components/ui/PlaidLink.tsx
+++ banking-app/components/ui/PlaidLink.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './button'
-import {PlaidLinkOnSuccess, PlaidLinkOptions} from 'react-plaid-link'
+import {PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink} from 'react-plaid-link'
 import { StyledString } from 'next/dist/build/swc';
 import { useRouter } from 'next/navigation';
 
@@ -32,11 +32,15 @@ const PlaidLink = ({user, variant}: PlaidLinkProps) => {
         onSuccess
         
     }
+    const {open, ready } = usePlaidLink(config);
   return (
     <>
     {
         variant === 'primary' ? (
-            <Button className='plaidlink-primary'>
+            <Button 
+            onClick={() => open()}
+            disabled={!ready}
+            className='plaidlink-primary'>
                 Connect bank
             </Button>
         ): variant === 'ghost' ? (
