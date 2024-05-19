b0b2583b00728f963e2b1b46c036e8d401814aea
mwihoti <danielmwihoti@gmail.com>
2024-05-19 12:03:14 +0300

    onSuccess PlaidLinkonSuccess

--- banking-app/components/ui/PlaidLink.tsx
+++ banking-app/components/ui/PlaidLink.tsx
@@ -1,10 +1,14 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './button'
-import {PlaidLinkOptions} from 'react-plaid-link'
+import {PlaidLinkOnSuccess, PlaidLinkOptions} from 'react-plaid-link'
+import { StyledString } from 'next/dist/build/swc';
+import { useRouter } from 'next/navigation';
 
 const PlaidLink = ({user, variant}: PlaidLinkProps) => {
 
     const [token, setToken] = useState();
+    const router = useRouter();
+
     useEffect (() => {
         const getLinkToken = async () => {
             //const data = await createLinkToken(user);
@@ -12,7 +16,16 @@ const PlaidLink = ({user, variant}: PlaidLinkProps) => {
         }
         getLinkToken();
     }, []);
-    const onSuccess = useCallback(async () => {}, [user])
+    const onSuccess = useCallback<PlaidLinkOnSuccess>(async (public_token:string) => {
+        // await exchangePublicToken({
+        //  publicToken: public_token,
+        //  user,
+
+        //})
+        router.push('/');
+    }, 
+    
+    [user])
 
     const config: PlaidLinkOptions = {
         token,
