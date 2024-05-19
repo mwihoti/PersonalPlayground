7ebff750315cec72c71ea7683368179e0306f6ac
mwihoti <danielmwihoti@gmail.com>
2024-05-19 11:51:13 +0300

    Plaid config onSuccess

--- banking-app/components/ui/PlaidLink.tsx
+++ banking-app/components/ui/PlaidLink.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './button'
+import {PlaidLinkOptions} from 'react-plaid-link'
 
 const PlaidLink = ({user, variant}: PlaidLinkProps) => {
+
+    const [token, setToken] = useState();
+    useEffect (() => {
+        const getLinkToken = async () => {
+            //const data = await createLinkToken(user);
+            //setToken(data?linkToken);
+        }
+        getLinkToken();
+    }, []);
+    const onSuccess = useCallback(async () => {}, [user])
+
+    const config: PlaidLinkOptions = {
+        token,
+        onSuccess
+        
+    }
   return (
     <>
     {
         variant === 'primary' ? (
-            <Button>
+            <Button className='plaidlink-primary'>
                 Connect bank
             </Button>
         ): variant === 'ghost' ? (
