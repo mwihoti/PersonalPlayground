c8f742193a6d48be11855a6202943e953819404a
mwihoti <danielmwihoti@gmail.com>
2024-05-30 10:25:38 +0300

    login form

--- microblog/app/config.py
+++ microblog/app/config.py
@@ -1,7 +1,8 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+# basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+   # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+     #   'sqlite:///' + os.path.join(basedir, 'app.db')
