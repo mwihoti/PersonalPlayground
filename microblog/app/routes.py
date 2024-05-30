4e649d4177ee2b4b10608590e49115e2ca7e15a5
mwihoti <danielmwihoti@gmail.com>
2024-05-30 09:08:37 +0300

    template inheritance

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,4 +5,14 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'Mwihoti'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
