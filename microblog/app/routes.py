0f0d32e05fb5666a83a58f988b0a38ca4b4a0234
mwihoti <danielmwihoti@gmail.com>
2024-06-01 12:49:32 +0300

    Login view form & routes

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -15,4 +17,11 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
+    
+
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
