22b08a0e9ca65a3c1894e7f0f9bb3e2881ce8a10
mwihoti <danielmwihoti@gmail.com>
2024-06-07 10:24:01 +0300

    password hashing and verification

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -3,6 +3,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
@@ -13,6 +14,12 @@ class User(db.Model):
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
