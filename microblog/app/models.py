2a1daf1c4bcb1dca903399e6db739de84361e97b
mwihoti <danielmwihoti@gmail.com>
2024-06-07 11:17:24 +0300

    User loader function

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,11 +1,13 @@
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import db
+from app import db, login
 from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
@@ -23,6 +25,11 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    @login.user_loader
+    def load_user(id):
+        return db.session.get(User, int(id))
+    
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
