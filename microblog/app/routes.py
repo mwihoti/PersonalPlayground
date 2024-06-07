1d10f1eeecaaa749b7464b46cf43cd7836a980a3
mwihoti <danielmwihoti@gmail.com>
2024-06-07 11:41:05 +0300

    log in users

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,10 @@
 from app import app
 from flask import render_template, redirect, flash, url_for
 from app.forms import LoginForm
+from app.models import User
+from app import db
+from flask_login import current_user, login_user
+import sqlachemy as sa
 
 
 @app.route('/')
@@ -23,9 +27,17 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or passworrd')
+            return redirect(url_for('login'))
+        login_user (user, remember=form.remember_me.data)
         return redirect (url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
