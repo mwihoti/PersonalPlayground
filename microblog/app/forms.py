91cd1f784a9ef835516beab4f98fda8b78c22bbd
mwihoti <danielmwihoti@gmail.com>
2024-06-10 21:42:53 +0300

    Fix duplicate user

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -34,4 +34,17 @@ class RegistrationForm(FlaskForm):
 class EditProfileForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     about_me = TextAreaField('About me',  validators=[Length(min=0, max=140)])
-    submit = SubmitField('Submit')     
+    submit = SubmitField('Submit')
+    
+    def __init__(self, original_username, **args, **kwargs):
+        super().__init__(*args, *kwargs)
+        self.original_username = original_username
+    
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == self.username.data
+            ))
+            if user is not None:
+                raise ValidationError("Please use a different username.")
+    
