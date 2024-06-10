697f6347ccda08db2f4a77abd254a43b0487577a
mwihoti <danielmwihoti@gmail.com>
2024-06-10 16:04:48 +0300

    custom error page

--- microblog/app/errors.py
+++ microblog/app/errors.py
@@ -5,7 +5,7 @@ from app import app, db
 def not_found_error(error):
     return render_template('404.html'), 404
 
-@app.error_handler(404)
+@app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
