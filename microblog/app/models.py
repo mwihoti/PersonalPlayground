f68a73fe49832da9087fb89bb2189ec58436be4c
mwihoti <danielmwihoti@gmail.com>
2024-06-10 14:55:46 +0300

    Edit profile, Last seen

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -16,6 +16,9 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
     
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
     
