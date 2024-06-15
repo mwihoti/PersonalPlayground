b99d2c345152625d3b00667adf6dd284ec19f25b
mwihoti <danielmwihoti@gmail.com>
2024-06-15 16:21:58 +0300

    Test user follow & unfollow

--- microblog/tests.py
+++ microblog/tests.py
@@ -41,3 +41,20 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(following, [])
         self.assertEqual(followers)
         
+        u1.follow(u2)
+        db.session.commit()
+        self.assertTrue(u1.is_following(u2))
+        self.assertEqual(u1.following_count(), 1)
+        self.assertEqual(u2.followers_count(), 1)
+        u1_following = db.session.scalars(u1.following.select()).all()
+        u2_followers = db.session.scalars(u2.followers.select()).all()
+        self.assertEqual(u1_following[0].username, 'Daniel')
+        self.assertEqual(u2_followers [0].username, 'Mike')
+        
+        u1.unfollow(u2)
+        db.session.commit()
+        self.assertFalse(u1.is_following(u2))
+        self.assertEqual(u1.following_count(), 0)
+        self.assertEqual(u2.followers_count(), 0)
+        
+        
