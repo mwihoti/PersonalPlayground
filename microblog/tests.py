eedc2de0958f92e5c9ed4931161b9eaea9a6192e
mwihoti <danielmwihoti@gmail.com>
2024-06-15 16:00:08 +0300

    test follow

--- microblog/tests.py
+++ microblog/tests.py
@@ -23,3 +23,21 @@ class UserModelCase(unittest.TestCase):
         u.set_password('pass1#')
         self.assertFalse(u.check_password('dog'))
         self.assertFalse(u.check_password('pass1#'))
+    
+    def test_avatar(self):
+        u = User(username='Daniel', email='daniel@gmail.com')
+        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128'))
+        
+    def test_follow(self):
+        u1 = User(username='Daniel', email='daniel@gmail.com')
+        u2 = User(username='Mike', email='mike@gmail.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        following = db.session.scalars(u1.following.select()).all()
+        followers = db.session.scalars(u2.followers.select()).all()
+        self.assertEqual(following, [])
+        self.assertEqual(followers)
+        
