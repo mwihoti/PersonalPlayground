0f453f34859fc6b262fa113728436879c8892ef6
mwihoti <danielmwihoti@gmail.com>
2024-06-16 22:13:04 +0300

    submission form routes

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template, redirect, flash, url_for, request
-from app.forms import LoginForm
-from app.models import User
+from app.forms import LoginForm, PostForm
+from app.models import User, Post
 from app import db
 from urllib.parse import urlsplit
 from app.forms import RegistrationForm
@@ -12,24 +12,33 @@ from datetime import datetime, timezone
 
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your Post is now live')
+        return redirect(url_for('index'))
+        
     #user = {'username': 'Mwihoti'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+   # posts = [
+       # {
+       #     'author': {'username': 'John'},
+       #     'body': 'Beautiful day in Portland!'
+      #  },
+    #    {
+     #       'author': {'username': 'Susan'},
+     #       'body': 'The Avengers movie was so cool!'
+     #   }
+   # ]
+   posts= db.session.scalars()
     
 
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home', posts=posts, form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
