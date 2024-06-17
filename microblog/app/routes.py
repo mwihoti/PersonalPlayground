93d3d2b1edf8b98e9c53026a1fc1ff83ef314103
mwihoti <danielmwihoti@gmail.com>
2024-06-17 20:24:42 +0300

    pagination for user page

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -91,12 +91,19 @@ def register():
 @login_required
 def user(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
-    posts = [
-        {'author': user, 'body':'Test post #1'},
-        {'author': user, 'body': 'Test Post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    query = user.posts.select().order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], 
+                        error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
+    
+     
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    
+    return render_template('user.html', user=user, posts=posts, form=form, prev_url=prev_url, next_url=next_url)
 
 @app.before_request
 def before_request():
