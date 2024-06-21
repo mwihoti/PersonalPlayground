0571a142683781b0a4d0a9ac41a7a067994f12b0
mwihoti <danielmwihoti@gmail.com>
2024-06-21 10:27:00 +0300

    Flask email server

--- microblog/config.py
+++ microblog/config.py
@@ -12,5 +12,5 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['danielmwihoti@gmail.com']
-    POSTS_PER_PAGE = 25
+    ADMINS = ['danmwihoti@gmail.com']
+    POSTS_PER_PAGE = 4
