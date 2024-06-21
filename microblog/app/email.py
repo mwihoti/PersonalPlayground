59139f881431cfd925178f7eedd7f662c2c130d0
mwihoti <danielmwihoti@gmail.com>
2024-06-21 17:21:59 +0300

    Asychronous email

--- microblog/app/email.py
+++ microblog/app/email.py
@@ -1,6 +1,8 @@
 from flask_mail import Message
 from app import mail, app
 from flask import render_template
+from threading import Thread
+
 
 def send_email(subject, sender, receipts, text_body, html_body):
     msg = Message(subject, sender=sender, receipts=receipts)
@@ -14,3 +16,13 @@ def send_password_reset_email(user):
                receipts=[user.email],
                text_body=render_template('email/reset_password.txt', user=user, token=token),
                html_body=render_template('email/reset_password.txt', user=user, token=token))
+    
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(subject, sender, receipts, text_body, html_body):
+    msg = Message(subject, sender, recipients=receipts)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_async_email, args=(app, msg)).start()
