41b79bf76533f8462c8f9f4fb441e93432eb7f58
mwihoti <danielmwihoti@gmail.com>
2024-06-21 11:57:56 +0300

    Send password reset_email

--- microblog/app/email.py
+++ microblog/app/email.py
@@ -1,8 +1,16 @@
 from flask_mail import Message
-from app import mail
+from app import mail, app
+from flask import render_template
 
 def send_email(subject, sender, receipts, text_body, html_body):
     msg = Message(subject, sender=sender, receipts=receipts)
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
+    
+def send_password_reset_email(user):
+    token = user.get_reset_password_token()
+    send_email('[Microblog ] Reset Your password', sender=app.config['ADMINS'][0], 
+               receipts=[user.email],
+               text_body=render_template('email/reset_password.txt', user=user, token=token),
+               html_body=render_template('email/reset_password.txt', user=user, token=token))
