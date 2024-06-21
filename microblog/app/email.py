7f6ef254e5af4bb189cf6461c248239366ad292d
mwihoti <danielmwihoti@gmail.com>
2024-06-21 19:49:28 +0300

    email server config

--- microblog/app/email.py
+++ microblog/app/email.py
@@ -4,25 +4,25 @@ from flask import render_template
 from threading import Thread
 
 
-def send_email(subject, sender, receipts, text_body, html_body):
-    msg = Message(subject, sender=sender, receipts=receipts)
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
+
     
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email('[Microblog ] Reset Your password', sender=app.config['ADMINS'][0], 
-               receipts=[user.email],
+               recipients=[user.email],
                text_body=render_template('email/reset_password.txt', user=user, token=token),
                html_body=render_template('email/reset_password.txt', user=user, token=token))
     
 def send_async_email(app, msg):
     with app.app_context():
-        mail.send(msg)
+        try:
+            mail.send(msg)
+        except Exception as e:
+            app.logger.error(f'Failed to send email: (e)')
 
-def send_email(subject, sender, receipts, text_body, html_body):
-    msg = Message(subject, sender, recipients=receipts)
-    msg.body = text_body
-    msg.html = html_body
-    Thread(target=send_async_email, args=(app, msg)).start()
--- microblog/config.py
+++ microblog/config.py
@@ -8,9 +8,9 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'app.db')
         
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 25))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['danmwihoti@gmail.com']
-    POSTS_PER_PAGE = 4
+    POSTS_PER_PAGE = 4  
