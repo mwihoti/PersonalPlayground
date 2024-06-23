ed922f4e181fe45d9971ff55b5d3acc50fdc9427
mwihoti <danielmwihoti@gmail.com>
2024-06-23 08:55:49 +0300

    Routes model pybabele

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,18 +1,19 @@
-from flask import Flask, request
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
+from config import Config
+
 
 def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
+    #return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'es'
 
 
 app = Flask(__name__)
@@ -21,9 +22,10 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 moment = Moment(app)
-babel = Babel(app, locale_selector=get_locale   )
+babel = Babel(app, locale_selector=get_locale)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -40,8 +42,7 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
-        
-    
+
     if not os.path.exists('logs'):
         os.mkdir('logs')
     file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
@@ -53,5 +54,6 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
 
 from app import routes, models, errors
