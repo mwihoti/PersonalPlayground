4c06dad4c1bd16c8a37096831f634d3878249959
mwihoti <danielmwihoti@gmail.com>
2024-06-23 06:46:15 +0300

    Marking text to translate

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -8,6 +8,10 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel
+
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 
@@ -19,6 +23,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 moment = Moment(app)
+babel = Babel(app, locale_selector=get_locale   )
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
--- microblog/config.py
+++ microblog/config.py
@@ -14,3 +14,4 @@ class Config:
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['danmwihoti@gmail.com']
     POSTS_PER_PAGE = 12  
+    LANGUAGES = ['en', 'es', 'sw']
