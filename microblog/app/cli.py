ccac9aa9b921e97293a232debced165f258b0932
mwihoti <danielmwihoti@gmail.com>
2024-06-23 10:50:58 +0300

    cli command init update & comile

--- microblog/app/cli.py
+++ microblog/app/cli.py
@@ -1,22 +1,36 @@
 from app import app
 import os
+import click
 
 @app.cli.group()
 def translate():
     """Translation and localization commands."""
     pass
+   
+@translate.command()
+@click.argument('lang')
+def init(lang):
+    """Initializes  a new language."""
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
+        raise RuntimeError('extract command failed')
+    if os.system('pybabel init -i messages.pot -d app/translations -l ' + lang):
+        raise RuntimeError('innit command failed')
+    os.remove('messages.pot')
+    
+        
 
 @translate.command()
 def update():
     """Update all languages"""
-    if os.system('pybabel extract -F babel.cfg -k -l -o message.pot'):
+    if os.system('pybabel extract -F babel.cfg -k _l -o messages.pot .'):
         raise RuntimeError('extract command failed')
-    if os.system('pybabel update -i message.pot -d app/translations'):
+    if os.system('pybabel update -i messages.pot -d app/translations'):
         raise RuntimeError('extract command failed')
-    os.remove('message.pot')
+    os.remove('messages.pot')
 
 @translate.command()
 def compile():
     """Compile all languages"""
     if os.system('pybabel compile -d app/translations'):
         raise RuntimeError('Compile command failed')
+ 
