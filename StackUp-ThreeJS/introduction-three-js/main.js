d0b11d4eefc804c212d0bc4dbf2f65ed2d709eee
mwihoti <danielmwihoti@gmail.com>
2024-06-25 10:46:12 +0300

    three D Cube box rotation

--- StackUp-ThreeJS/introduction-three-js/main.js
+++ StackUp-ThreeJS/introduction-three-js/main.js
@@ -12,9 +12,29 @@ document.getElementById('app').appendChild(renderer.domElement)
 // Create a geometry (a cube in this case)
 const geometry = new THREE.BoxGeometry();
 const  material = new THREE.MeshPhongMaterial({color: 0x00ff00});
-const cube = new THREE.MESH(geometry, material);
+const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 
 // create point of light
+const pointLight =  new THREE.PointLight(0xffffff)
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
 
+// Create ambient Light
+const ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+
+function animate () {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+
+  renderer.render(scene, camera);
+}
+
+animate()
+dan_m-stackup-threejs-intro
