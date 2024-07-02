e3a3554e518786540a7939a8a83eb4d0c77ef165
mwihoti <danielmwihoti@gmail.com>
2024-07-02 08:13:45 +0300

    Vercel configurations

--- hello-api/server.js
+++ hello-api/server.js
@@ -2,26 +2,28 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-    res.send('Welcome to the API. Use the endpoint /api/hello?visitor_name=YourName to get a personalized greeting.');
+    res.send('Welcome. We are using endpoint /api/hello?visitor_name=YourName to get a personalized greeting.');
 });
 
 app.get('/api/hello', async (req, res) => {
-    const userName = req.query.visitor_name  || 'Guest';
+    const userName = req.query.visitor_name || 'Guest';
 
     const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
     const location = 'Kenya';
-
     const temp = 12;
 
-    res.json ({
+    res.json({
         client_ip: clientIp,
         location: location,
-        greeting: `Hello, ${userName}, the temperature is ${temp}`
-    })
-})
+        greeting: `Hello, ${userName}!, the temperature is ${temp} degrees Celsius in ${location}`
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Server is runing on ${port}`)
-})
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
