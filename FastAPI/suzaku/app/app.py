92300483ad7335c3801a2944187c594d6d65a102
mwihoti <danielmwihoti@gmail.com>
2024-07-09 20:15:11 +0300

    filtering data

--- FastAPI/suzaku/app/app.py
+++ FastAPI/suzaku/app/app.py
@@ -1,10 +1,39 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+username = "Mwihoti"
+description = "I love developing web applications."
+description += "And this text was created with Jinja"
+
+projects = [
+    {
+        'id': 1,
+        'title': 'E-commerce Website',
+        'description': 'A website where you can buy stuff',
+        'author': 'Mwihoti',
+        'category': 'Web Development'
+    },
+    {
+        'id': 2,
+        'title': 'Flask Website',
+        'description': 'A website made with Flask',
+        'author': 'Mwihoti',
+        'category': 'Web Development'
+    },
+    {
+        'id': 3,
+        'title': 'Cool Mobile App',
+        'description': 'An app that does cool stuff',
+        'author': 'Mwihoti',
+        'category': 'Mobile Development'
+    }
+]
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html', username=username, description=description, projects = projects)
 
 @app.route('/about')
 def about():
@@ -19,5 +48,5 @@ def contact():
     
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
