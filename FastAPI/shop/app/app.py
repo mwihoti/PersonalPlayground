0d1f1f01da11b98174988079ef2b4780c336cc63
mwihoti <danielmwihoti@gmail.com>
2024-07-11 10:46:22 +0300

    vercel config file

--- FastAPI/shop/app/app.py
+++ FastAPI/shop/app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
-from models import Product, User, db
-from controllers.users import get_users, get_user, add_user, update_user, delete_user, get_user_by_username
-from controllers.products import get_products, get_product, add_product, update_product, delete_product
+from app.models import Product, User, db
+from app.controllers.users import get_users, get_user, add_user, update_user, delete_user, get_user_by_username
+from app.controllers.products import get_products, get_product, add_product, update_product, delete_product
 from sqlalchemy import exc
 import hashlib
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
