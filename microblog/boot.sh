82cf982642f05a0e573a0beff00a50d3d4b062b8
mwihoti <danielmwihoti@gmail.com>
2024-07-16 16:09:18 +0300

    database migrations for flask upgrade

--- microblog/boot.sh
+++ microblog/boot.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-flask db upgrade
+while true; do
+    flask db upgrade
+    if [["$?" == "0"]]; then
+        break
+    fi
+    echo upgrade command failed, retrying in 5 secs...
+done
 exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
