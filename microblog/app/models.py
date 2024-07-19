edf0604bde44358ae0413cc6c93612275e317fac
mwihoti <danielmwihoti@gmail.com>
2024-07-19 10:01:52 +0300

    database model for private messages

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -173,3 +173,11 @@ class Post(SearchableMixin, db.Model):
     
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+class Message(db.model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    sender_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    recipient_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
