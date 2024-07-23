53ce907938542ac4c259f57ac19c4dfdfbef2802
mwihoti <danielmwihoti@gmail.com>
2024-07-23 14:06:00 +0300

    class Task model

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -12,7 +12,8 @@ from app import db, login
 from flask import current_app
 import json
 from app.search import add_to_index, remove_from_index, query_index
-
+import redis
+import rq
 
 class SearchableMixin(object):
     @classmethod
@@ -98,6 +99,8 @@ class User(UserMixin, db.Model):
         foreign_keys='Message.recipient_id', back_populates='recipient'
     )
     notifications: so.WriteOnlyMapped['Notification'] = so.relationship(back_populates='user')
+
+    tasks: so.WriteOnlyMapped['Task'] = so.relationship(back_populates='user')
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -223,3 +226,23 @@ class Notification(db.model):
 
     def get_data(self):
         return json.loads(str(self.payload_json))
+
+class Task(db.Model):
+    id: so.Mapped[str] = so.mapped_column(sa.String(36), primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(128), index=True)
+    description: so.Mapped[str] = so.mapped_column(sa.String(128))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id))
+    complete: so.Mapped[bool] = so.mapped_column(default=False)
+
+    user: so.Mapped[User] = so.relationship(back_populates='tasks')
+
+    def get_rq_job(self):
+        try:
+            rq_job = rq.job.Job.fetch(self.id, connection=current_app.redis)
+        except (redis.exceptions.RedisError, rq.exceptions.NoSuchJobError):
+            return None
+        return rq_job
+    
+    def get_progress(self):
+        job = self.get_rq_job()
+        return job.meta.get('progress', 0) if job is not None else 100
