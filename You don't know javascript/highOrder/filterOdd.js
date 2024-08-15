d4c0f2d234c06bdb2954e1ad7a6cbddfd23ad9bb
mwihoti <danielmwihoti@gmail.com>
2024-08-15 09:41:08 +0300

    up

--- You don't know javascript/highOrder/filterOdd.js
+++ You don't know javascript/highOrder/filterOdd.js
@@ -1,5 +1,5 @@
 // use filter() to return only the odd numbers from an array of numbers.
 const arr = [1, 2, 3, 4, 5, 6, 7]
 
-const getOdd = arr.filter((num) => num % 2)
+const getOdd = arr.filter((num) => num % 2 == 0)
 console.log(getOdd)
