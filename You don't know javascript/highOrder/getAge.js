4733259c2a003fc670dc17aa96919cceaecaacd4
mwihoti <danielmwihoti@gmail.com>
2024-08-15 09:56:19 +0300

    filter branch

---
+++ You don't know javascript/highOrder/getAge.js
@@ -0,0 +1,11 @@
+// use filter() to return only the users having age greater than 30 in an array.
+const users = [
+    {firstName: 'John', lastName: 'Doe', age: 25},
+    {firstName: 'Jane', lastName: 'Doe', age: 30},
+    {firstName: 'Jack', lastName: 'Doe', age: 35},
+    {firstName: 'Jill', lastName: 'Doe', age: 40},
+    {firstName: 'Joe', lastName: 'Doe', age: 45},
+]
+
+const getAge = users.filter(({age} )=> age <= 30)
+console.log(getAge)
