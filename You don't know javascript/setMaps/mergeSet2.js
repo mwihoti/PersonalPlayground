bfe8da4234dd5043eb5499405f7cf0013a164989
Danmwihoti <danielmwihoti@gmail.com>
2024-08-20 23:41:34 +0300

    myMap and mySet functions initialized

--- You don't know javascript/setMaps/mergeSet2.js
+++ You don't know javascript/setMaps/mergeSet2.js
@@ -1,5 +1,11 @@
 function MergeSet(set, map) {
-    return Map(
-        [...set].map(key=> [key, map.get(key) || undefined])
+    return new Map(
+        [...set].map(key => [key, map.get(key) || 'undefined'])
     )
 }
+
+let myMap = new Set ([1, 2, 3, 4, 5, 6])
+let mySet = new Map([[1, 'one'], [2, 'two'], [3, 'three'], [4, 'four'], [5, 'five']])
+
+let merged = MergeSet(myMap, mySet)
+console.log(merged)
