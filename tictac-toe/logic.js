6f3413d9bac076ac19e3c411a258714f269628c5
mwihoti <danielmwihoti@gmail.com>
2024-11-02 18:00:58 +0300

    check invalid moves

--- tictac-toe/logic.js
+++ tictac-toe/logic.js
@@ -18,3 +18,12 @@ $("button").click(function () {
         turn = 1;
     }
 })
+
+$("button").click(function () {
+    if ($(this).hasClass("fa fa-times") || $(this).hasClass("fa fa-check")) {
+        $(this).css("background-color", "red");
+        setTimeout( () => {
+            $(this).css("background-color", "white");
+        }, 800);
+    }
+})
