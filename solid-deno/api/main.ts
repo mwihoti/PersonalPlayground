605e2e796f7c7db4c78d62ed6641fe2274388068
mwihoti <danielmwihoti@gmail.com>
2024-12-07 10:30:05 +0300

    get started with deno

--- solid-deno/api/main.ts
+++ solid-deno/api/main.ts
@@ -13,6 +13,22 @@ app.get("/api/dinosaurs", (c) => {
 
 app.get("api/dinosaurs/:dinosaur", (c) => {
     if (!c.req.param("dinosaur")) {
-        return c.text("No dinosaur name provided")
+        return c.text("No dinosaur name provided.");
     }
-})
+
+    const dinosaur = data.find((item) =>
+        item.name.toLowerCase() === c.req.param("dinosaur").toLowerCase()
+
+    );
+
+    console.log(dinosaur);
+
+    if (dinosaur) {
+        return c.json(dinosaur);
+    } else {
+        return c.notFound();
+    }
+
+});
+
+Deno.serve(app.fetch)
--- solid-deno/vite.config.ts
+++ solid-deno/vite.config.ts
@@ -3,4 +3,12 @@ import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+      }
+    }
+  }
 })
