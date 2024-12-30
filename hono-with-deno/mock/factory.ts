ad8908cadfcaf0d80cdb2cf4204853d8fb99b0a5
mwihoti <danielmwihoti@gmail.com>
2024-12-30 21:43:36 +0300

    factory.s has two mock data genarators. the animalFactory and PersonalFacory

--- hono-with-deno/mock/factory.ts
+++ hono-with-deno/mock/factory.ts
@@ -1,5 +1,6 @@
 import { factory } from '@findhow/zod-factory';
 import AnimalSchema from './animal.schema.ts';
+import PersonalDetailSchema from "./personal-details.schema.ts";
 
 
 const AnimalFactory  = factory(AnimalSchema, (faker) => ({
@@ -9,4 +10,15 @@ const AnimalFactory  = factory(AnimalSchema, (faker) => ({
 }
 ));
 
-export default AnimalFactory;
+const PersonalDetailFactory = factory(PersonalDetailSchema, (faker) => ({
+    id: faker.number.int(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  }));
+
+  const factoryAnimals = AnimalFactory.createMany(100);
+  const factoryPeople = PersonalDetailFactory.createMany(100);
+  
+  export { AnimalFactory, factoryAnimals, factoryPeople, PersonalDetailFactory };
