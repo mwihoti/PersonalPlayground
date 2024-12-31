29b996825b6da5b11a7f8634591a1bc21b1375b3
mwihoti <danielmwihoti@gmail.com>
2024-12-31 14:16:49 +0300

    Add test to verify password hashes with respective salts using argon2Verify

---
+++ hono-with-deno/mock/mock_test.ts
@@ -0,0 +1,28 @@
+import { argon2Verify } from "../auth/pwd.ts";
+import { generateMockDataForDb } from "./generateMockDatabase.ts";
+import { factoryPeople } from "./factory.ts";
+import { assert } from "@std/assert";
+import { PersonalDetailWithSalt } from "../types/common.ts";
+import personalDetailsDb from './database/personal-details.db';
+
+
+Deno.test("All password hashes are verified with their respective salt.", () => {
+    const personalDetailsDbPath = "./mock/database/people-mock.db";
+    const animalMockDbPath = "./mock/database/animal-mock.db";
+    const { personalDetailsMockDb } = generateMockDataForDb(
+        personalDetailsDbPath, 
+        animalMockDbPath,
+    );
+
+    factoryPeople.forEach((person) => {
+        const stmt = personalDetailsMockDb.prepare(
+            `
+					SELECT * FROM people WHERE email = '${person.email}';
+					`,
+        );
+        const row = stmt.get<PersonalDetailWithSalt>();
+        assert(row !== undefined);
+
+        assert(argon2Verify(person.password, row.password, row.salt));
+    });
+});
