3baff62ca5969cab7bf72f7162f07189c073437c
mwihoti <danielmwihoti@gmail.com>
2024-12-31 11:27:24 +0300

    Add argon2 password verification with base64 encoding and environment variable support

---
+++ hono-with-deno/auth/pwd.ts
@@ -0,0 +1,23 @@
+import { decodeBase64, encodeBase64 } from "@std/encoding";
+import { type Argon2Params, hash } from "@denosaurs/argontwo";
+import "@std/dotenv/load";
+
+const TOKEN = decodeBase64(Deno.env.get("API_KEY") ?? "");
+const encoder = new TextEncoder();
+
+export const argon2Verify = (
+    password: string,
+    salt: string,
+    originalHash: string,
+): boolean => {
+    const encodedPassword = encoder.encode(password);
+    const encodedSalt = encoder.encode(salt);
+    const params: Argon2Params = {
+        algorithm: "Argon2id",
+        secret: TOKEN,
+        version: 0x13,
+    };
+
+    const ret = encodeBase64(hash(encodedPassword, encodedSalt, params)) === originalHash;
+    return ret;
+}
