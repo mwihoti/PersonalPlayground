276e028d6c74c9a77734b01a623ec43f2006e1ec
mwihoti <danielmwihoti@gmail.com>
2024-12-31 08:47:55 +0300

    Initialize SQLite database and create personalDetails table if not exists

---
+++ hono-with-deno/mock/database/personal-details.db.ts
@@ -0,0 +1,16 @@
+import { Database } from "@db/sqlite";
+const personalDetailsDb = new Database("./mock/database/people.db");
+
+
+export const create_personal_details_table_command = `
+            CREATE TABLE IF NOT EXISTS personal_details (
+            id INTEGER PRIMARY KEY,
+            firstName TEXT NOT NULL,
+            lastName TEXT NOT NULL,
+            email TEXT NOT NULL,
+            password TEXT NOT NULL,
+            salt TEXT NOT NULL);
+
+`;
+
+export default personalDetailsDb; 
